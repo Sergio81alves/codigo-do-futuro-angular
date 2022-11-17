2fd39ab643e37f7038b9587e01302fc03ed98044
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2022-11-17 17:40:45 -0300

    começo do curriculo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentHeaderComponent } from './component-header/component-header.component';
+import { MainComponent } from './main/main.component';
+import { ComponentSidebarComponent } from './component-sidebar/component-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentHeaderComponent,
+    MainComponent,
+    ComponentSidebarComponent
   ],
   imports: [
     BrowserModule,
